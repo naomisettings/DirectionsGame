ae36dd4c801bca7c026358da60eb2f449378e37c
sellares.feiner.raimon <sellares.feiner.raimon@alumnat.copernic.cat>
2021-06-07 18:30:52 +0200

    define class box

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import pygame
 import game.game_config as gc
+from game.box import Box
+
 
 from pygame import display, event, image
 
@@ -12,6 +14,12 @@ screen.blit(pygame.transform.scale(board, screen.get_size()), (0, 0))
 
 #board = pygame.transform.scale(board, (720, 720))
 #board_rect = board.get_rect(center=(1024,560))
+
+tiles = [Box(i) for i in range(0, gc.NUM_TILES_TOTAL)]
+x = 0
+#for (x in range(gc.NUM_TILES_TOTAL))
+
+
 
 running = True
 
@@ -26,5 +34,15 @@ while running:
             if e.key == pygame.K_ESCAPE:
                 running = False
 
-    #screen.blit(board, board_rect)
+
+    # Display boxes
+    screen.fill((255, 255, 255))
+"""
+    for i, tile in enumerate(tiles):
+        current_image = tile.image if i in current_images_displayed else tile.box
+        if not tile.skip:
+            screen.blit(current_image, (tile.col * gc.IMAGE_SIZE + gc.MARGIN, tile.row * gc.IMAGE_SIZE + gc.MARGIN))
+        else:
+            total_skipped += 1
     display.flip()
+"""
--- game/game_config.py
+++ game/game_config.py
@@ -1,8 +1,26 @@
 import os
 
 IMAGE_SIZE = 128
-SCREEN_SIZE = 512
-NUM_TILES_SIDE = 4
-NUM_TILES_TOTAL = 16
 MARGIN = 8
+
+NUM_TILES_BORDER = 11
+NUM_TILES_MIDDLE = 5
+NUM_TILES_CENTER = 1
+
+NUM_TILES_SIDE = 5
+NUM_TILES_TOTAL = 25
+
+
+ASSET_DIR_BORDER = 'assets/border'
+ASSET_DIR_MIDDLE = 'assets/middle'
+ASSET_DIR_CENTER = 'assets/center'
+
+ASSET_FILES_BORDER = [x for x in os.listdir(ASSET_DIR_BORDER) if x[-3:].lower() == 'png']
+ASSET_FILES_MIDDLE = [x for x in os.listdir(ASSET_DIR_MIDDLE) if x[-3:].lower() == 'png']
+ASSET_FILES_CENTER = [x for x in os.listdir(ASSET_DIR_CENTER) if x[-3:].lower() == 'png']
+
+assert len(ASSET_FILES_BORDER) == 11
+assert len(ASSET_FILES_MIDDLE) == 5
+assert len(ASSET_FILES_CENTER) == 2
+
 
