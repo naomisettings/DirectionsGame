622524221e069e75fed97b37b24bdca76dd64144
sellares.feiner.raimon <sellares.feiner.raimon@alumnat.copernic.cat>
2021-06-08 13:02:50 +0200

    board distribution ok

--- box.py
+++ box.py
@@ -7,36 +7,49 @@ from pygame import image, transform
 box_border = [a for a in gc.ASSET_FILES_BORDER]
 box_middle = [a for a in gc.ASSET_FILES_MIDDLE]
 box_center = [a for a in gc.ASSET_FILES_CENTER]
-
-def available_boxes():
-    box_counts = []
-    box_counts.append(box_border)
-    box_counts.append(box_middle)
-    box_counts.append(box_center)
-    print(random.choice(box_counts)[0])
-
-    return box_counts
+box_start_end = [a for a in gc.ASSET_FILES_START_END]
 
 class Box:
     def __init__(self, index):
 
         self.index = index
-        self.name = random.choice(available_boxes())[0]
         
+        self.row = index // gc.NUM_TILES_SIDE
+        self.col = index % gc.NUM_TILES_SIDE
+        
+        name = ''
+        if (self.row == 4 and self.col == 0):
+            name = '0.png'
+        elif (self.row == 0 and self.col == 4):
+            name = '1.png'
+        elif (self.row == 0 or self.col == 0 or self.row == 4 or self.col == 4):
+            name = random.sample(box_border, gc.NUM_TILES_BORDER)[0]
+        elif (self.row == 1 or self.col == 1 or self.row == 3 or self.col == 3):
+            name = random.sample(box_middle, gc.NUM_TILES_MIDDLE)[0] 
+        else:
+            name = box_center[0]
+
+        self.name = name
+
         self.image_path_border = os.path.join(gc.ASSET_DIR_BORDER, self.name)
         self.image_path_middle = os.path.join(gc.ASSET_DIR_MIDDLE, self.name)
         self.image_path_center = os.path.join(gc.ASSET_DIR_CENTER, self.name)
-        self.row = index // gc.NUM_TILES_SIDE
-        self.col = index % gc.NUM_TILES_SIDE
+        self.image_path_start_end = os.path.join(gc.ASSET_DIR_START_END, self.name)
+
+
         self.skip = False
         
-        if (self.name in box_border):
+        if (self.name in box_start_end):
+            self.image_start_end = image.load(self.image_path_start_end)
+            self.image = self.image_start_end
+        elif (self.name in box_border):
             self.image_border = image.load(self.image_path_border)
             self.image = self.image_border
         elif (self.name in box_middle):
             self.image_middle = image.load(self.image_path_middle)
             self.image = self.image_middle
         else:
+            print(self.image_path_center)
             self.image_center = image.load(self.image_path_center)
             self.image = self.image_center
 
--- game_config.py
+++ game_config.py
@@ -3,7 +3,7 @@ import os
 IMAGE_SIZE = 122
 MARGIN = 6
 
-NUM_TILES_BORDER = 11
+NUM_TILES_BORDER = 9
 NUM_TILES_MIDDLE = 5
 NUM_TILES_CENTER = 1
 
@@ -14,13 +14,16 @@ NUM_TILES_TOTAL = 25
 ASSET_DIR_BORDER = 'assets\\border'
 ASSET_DIR_MIDDLE = 'assets\\middle'
 ASSET_DIR_CENTER = 'assets\\center'
+ASSET_DIR_START_END = 'assets\\start_end'
 
-ASSET_FILES_BORDER = [x for x in os.listdir(ASSET_DIR_BORDER) if x[-3:].lower() == 'png']
-ASSET_FILES_MIDDLE = [x for x in os.listdir(ASSET_DIR_MIDDLE) if x[-3:].lower() == 'png']
-ASSET_FILES_CENTER = [x for x in os.listdir(ASSET_DIR_CENTER) if x[-3:].lower() == 'png']
+ASSET_FILES_BORDER = [x for x in os.listdir(ASSET_DIR_BORDER)]
+ASSET_FILES_MIDDLE = [x for x in os.listdir(ASSET_DIR_MIDDLE)]
+ASSET_FILES_CENTER = [x for x in os.listdir(ASSET_DIR_CENTER)]
+ASSET_FILES_START_END = [x for x in os.listdir(ASSET_DIR_START_END)]
 
-assert len(ASSET_FILES_BORDER) == 11
+
+assert len(ASSET_FILES_BORDER) == 9
 assert len(ASSET_FILES_MIDDLE) == 5
 assert len(ASSET_FILES_CENTER) == 1
-
+assert len(ASSET_FILES_START_END) == 2
 
