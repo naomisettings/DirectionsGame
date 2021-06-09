fbbd59e6c76c05fe3de8a98cf88030ce4b3d6897
sellares.feiner.raimon <sellares.feiner.raimon@alumnat.copernic.cat>
2021-06-09 12:04:25 +0200

    code organtization

--- app.py
+++ app.py
@@ -4,13 +4,36 @@ from box import Box
 
 from pygame import display, event, image
 
-
 def find_index_from_xy(x, y):
     row = y // gc.IMAGE_SIZE
     col = x // gc.IMAGE_SIZE
     index = row * gc.NUM_TILES_SIDE + col
     return row, col, index
 
+
+def rotation_of_boxes(tiles):
+    moved = False
+    for i, tile in enumerate(tiles):
+        if (not(tile.row == 0 and tile.col == 4) and not(tile.row == 4 and tile.col == 0)):
+            if tile.row == 0 and tile.col != 3 and moved == False:
+                tile.col += 1
+                moved = True
+            elif tile.col == 4 and tile.row != 0 and tile.row != 4 and moved == False:
+                tile.row += 1
+                moved = True
+            elif tile.row == 4 and tile.col > 1 and moved == False:
+                tile.col -= 1
+                moved = True
+            elif tile.col == 0 and tile.row != 4 and tile.row != 0 and moved == False:
+                tile.row -= 1
+            elif tile.row == 0 and tile.col == 3 and moved == False:
+                tile.row = 1
+                tile.col = 4
+            elif tile.row == 4 and tile.col == 1 and moved == False:
+                tile.row = 3
+                tile.col = 0
+        moved = False
+        
 pygame.init()
 display.set_caption('Directions')
 screen = display.set_mode((0, 0), pygame.FULLSCREEN)
@@ -26,9 +49,9 @@ count_middle = 0
 
 for i in range(0, gc.NUM_TILES_TOTAL):
     tiles.append(Box(i, count_border, count_middle))
-    if i == 0 or i == 1 or i== 2 or i == 3 or i == 5 or i == 9 or i == 10 or i == 14 or i == 15 or i == 19 or i == 21 or i == 22 or i == 23 or i == 24:
+    if i == 0 or i == 1 or i == 2 or i == 3 or i == 5 or i == 9 or i == 10 or i == 14 or i == 15 or i == 19 or i == 21 or i == 22 or i == 23 or i == 24:
         count_border += 1
-    elif i == 6 or i == 7 or i== 8 or i == 9 or i == 11 or i == 13 or i == 16 or i == 17 or i == 18:
+    elif i == 6 or i == 7 or i == 8 or i == 9 or i == 11 or i == 13 or i == 16 or i == 17 or i == 18:
         count_middle += 1
 
     print(tiles[i].name)
@@ -49,34 +72,17 @@ while running:
         if e.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             row, col, index = find_index_from_xy(mouse_x, mouse_y)
-            moved = False
+
+            rotation_of_boxes(tiles)
+
             for i, tile in enumerate(tiles):
-                if (not(tile.row == 0 and tile.col == 4) and not(tile.row ==  4 and tile.col == 0)):
-                    if tile.row == 0 and tile.col != 3 and moved == False:   
-                        tile.col += 1
-                        moved = True
-                    elif tile.col == 4 and tile.row != 0 and tile.row != 4 and moved == False:
-                        tile.row += 1 
-                        moved = True
-                    elif tile.row == 4 and tile.col > 1 and moved == False:
-                        tile.col -= 1
-                        moved = True
-                    elif tile.col == 0 and tile.row != 4 and tile.row != 0 and moved == False:
-                        tile.row -= 1
-                    elif tile.row == 0 and tile.col == 3:
-                        tile.row = 1
-                        tile.col = 4
-                    elif tile.row == 4 and tile.col == 1:
-                        tile.row = 3
-                        tile.col = 0
-                moved = False    
-            for i, tile in enumerate(tiles):
-                screen.blit(tile.image, (tile.col * gc.IMAGE_SIZE + gc.MARGIN + 620, tile.row * gc.IMAGE_SIZE + gc.MARGIN + 165))
-    #Display boxes
+                screen.blit(tile.image, (tile.col * gc.IMAGE_SIZE +
+                            gc.MARGIN + 620, tile.row * gc.IMAGE_SIZE + gc.MARGIN + 165))
+    # Display boxes
     #screen.fill((255, 255, 255))
     screen.blit(board, [0, 0])
-  
-    for i, tile in enumerate(tiles):
-        screen.blit(tile.image, (tile.col * gc.IMAGE_SIZE + gc.MARGIN + 620, tile.row * gc.IMAGE_SIZE + gc.MARGIN + 165))
-    display.flip()
 
+    for i, tile in enumerate(tiles):
+        screen.blit(tile.image, (tile.col * gc.IMAGE_SIZE +
+                    gc.MARGIN + 620, tile.row * gc.IMAGE_SIZE + gc.MARGIN + 165))
+    display.flip()
